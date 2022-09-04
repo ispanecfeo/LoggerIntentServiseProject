230ed5b99ba080278e18ca08eb464c11fb8ad9d7
Stas Avdeev <ispanecfeo@gmail.com>
2022-09-04 22:05:33 +0300

    git init

---
+++ app/src/main/java/ru/gb/loggerintentserviceproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.gb.loggerintentserviceproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gb.loggerintentserviseproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
